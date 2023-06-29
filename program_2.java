649d54dc7ab492336af41aafd9aa6f9b37b5ffc4
Семён Курочкин <sema.kurochkin@mail.ru>
2023-06-29 16:19:40 +0300

    Задача 2

---
+++ program_2.java
@@ -0,0 +1,73 @@
+/*
+Даны два Deque, представляющие два целых числа. Цифры хранятся в обратном порядке и каждый из их узлов содержит одну цифру.
+2) Сложите два числа и верните сумму в виде связанного списка.
+Одно или два числа могут быть отрицательными.
+ */
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class program_2 {
+ public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in, "cp866");
+    System.out.println("Первое число Deque: ");
+    Deque<Integer> deque_1 = Str_to_deq(sc.nextLine());
+    System.out.println(deque_1);
+    System.out.println("Второе число Deque: ");
+    Deque<Integer> deque_2 = Str_to_deq(sc.nextLine());
+    System.out.println(deque_2);
+    sc.close();
+    Product(deque_1, deque_2);
+ }
+
+public static Deque<Integer> Str_to_deq(String input) {
+    Deque<Integer> deque = new ArrayDeque<>();
+    char[] inChar = input.toCharArray();
+    if (input.charAt(0) != '-') {
+        
+        for (char a : inChar) {
+             deque.addFirst(Integer.parseInt(String.valueOf(a)));
+        }
+    } else {
+        deque.addFirst(Integer.parseInt(String.valueOf(input.charAt(1))) * -1);
+        if (inChar.length > 2) {
+            for (int i = 2; i < inChar.length; i++) {
+                deque.addFirst(Integer.parseInt(String.valueOf(inChar[i])));
+            }
+        }
+    }
+    return deque;
+}
+    
+
+    public static Integer Deq_to_int(Deque<Integer> deque) {
+    int result = 0;
+    if (deque.getLast() > 0) {
+        for (int i = deque.size() - 1; i >= 0; --i) {
+            int pos = (int) Math.pow(10, i);
+            result += deque.pollLast() * pos;
+        }
+    } else {
+        result += deque.pollLast() * Math.pow(10, deque.size());
+        for (int i = deque.size() - 1; i >= 0; --i) {
+            int pos = (int) Math.pow(10, i);
+            result += (deque.pollLast() * -1) * pos;
+        }
+    }
+    return result;
+    }
+
+
+    public static void Product(Deque<Integer> deque_1, Deque<Integer> deque_2) {
+        LinkedList<Integer> resLinkedList = new LinkedList<>();
+        Integer Itog = Deq_to_int(deque_1) + Deq_to_int(deque_2);
+        while (Itog != 0) {
+            resLinkedList.addFirst(Itog % 10);
+            Itog /= 10;
+        }
+        System.out.print("Результат: ");
+        System.out.println(resLinkedList);
+    }
+ }
